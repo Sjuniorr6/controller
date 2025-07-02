2c589e4fc1ff0ffa193547d83e813d23178c2a39
Sjuniorr6 <sjuniorr6@gmail.com>
2025-07-02 13:00:03 -0300

    teste

--- scanner/models.py
+++ scanner/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Equipamento(models.Model):
     codigo = models.CharField(max_length=100)
     data_recebido = models.DateTimeField(auto_now_add=True)
-    peso_liquido = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    peso_bruto = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    peso_liquido = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
+    peso_bruto = models.DecimalField(max_digits=10, decimal_places=3, null=True, blank=True)
     medidas_caixa = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
