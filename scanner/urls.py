6ca716aba86414301afa7b93bd54ecc3587f60e4
Sjuniorr6 <sjuniorr6@gmail.com>
2025-07-02 10:22:19 -0300

    teste

--- scanner/urls.py
+++ scanner/urls.py
@@ -2,7 +2,6 @@
 
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('registrar/', views.registrar_equipamento, name='registrar_equipamento'),
--- scanner/views.py
+++ scanner/views.py
@@ -7,6 +7,7 @@ from .models import Equipamento
 def index(request):
     return render(request, 'scanner/scan.html')
 
+
 @csrf_exempt
 def registrar_equipamento(request):
     if request.method == 'POST':
@@ -14,7 +15,11 @@ def registrar_equipamento(request):
         codigo = data.get('codigo')
         if codigo:
             equipamento, criado = Equipamento.objects.get_or_create(codigo=codigo)
-            return JsonResponse({'status': 'ok', 'criado': criado})
+            return JsonResponse({
+                'status': 'ok',
+                'mensagem': 'Equipamento registrado com sucesso.' if criado else 'Equipamento já registrado.',
+                'criado': criado
+            })
         else:
             return JsonResponse({'status': 'erro', 'mensagem': 'Código inválido'}, status=400)
     return JsonResponse({'status': 'erro', 'mensagem': 'Método não permitido'}, status=405)
